775e20756f8b8049745258b723d648ee7591076b
spagabtti <spagabtti@gmail.com>
2017-12-29 21:49:06 -0800

    edit

--- app/src/main/java/com/spaga/videodemo/MainActivity.java
+++ app/src/main/java/com/spaga/videodemo/MainActivity.java
@@ -2,6 +2,7 @@ package com.spaga.videodemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.MediaController;
 import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +14,10 @@ public class MainActivity extends AppCompatActivity {
 
         VideoView videoView = findViewById(R.id.videoView);
         videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.Missionary1);
+
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
         videoView.start();
     }
 }
